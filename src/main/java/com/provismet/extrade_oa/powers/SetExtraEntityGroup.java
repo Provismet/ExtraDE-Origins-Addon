ec36e629d6895576bb03cd516c23ddffdf69db36
Provismet <17149901+Provismet@users.noreply.github.com>
2023-10-24 15:21:20 +0100

    Update to 1.20.2

--- src/main/java/com/provismet/extrade_oa/powers/SetExtraEntityGroup.java
+++ src/main/java/com/provismet/extrade_oa/powers/SetExtraEntityGroup.java
@@ -7,21 +7,24 @@ import io.github.apace100.apoli.power.PowerType;
 import io.github.apace100.apoli.power.SetEntityGroupPower;
 import io.github.apace100.apoli.power.factory.PowerFactory;
 import io.github.apace100.calio.data.SerializableData;
+import io.github.apace100.calio.data.SerializableDataTypes;
 import net.minecraft.entity.EntityGroup;
 import net.minecraft.entity.LivingEntity;
 
 public class SetExtraEntityGroup extends SetEntityGroupPower {
-    public SetExtraEntityGroup(PowerType<?> type, LivingEntity entity, EntityGroup group) {
-        super(type, entity, group);
+    public SetExtraEntityGroup(PowerType<?> type, LivingEntity entity, EntityGroup group, int priority) {
+        super(type, entity, group, priority);
     }
 
     @SuppressWarnings("rawtypes")
     public static PowerFactory createFactory () {
         return new PowerFactory<>(ExtraDE_OA.powerID("entity_group"),
-        new SerializableData()
-            .add("group", SerializableDataTypeOverride.ENTITY_GROUP),
-        data ->
-            (type, player) -> new SetEntityGroupPower(type, player, (EntityGroup)data.get("group")))
-        .allowCondition();
+            new SerializableData()
+                .add("group", SerializableDataTypeOverride.ENTITY_GROUP)
+                .add("priority", SerializableDataTypes.INT, 0),
+        data -> (type, player) -> new SetEntityGroupPower(type, player,
+            (EntityGroup)data.get("group"),
+            data.getInt("priority")
+        )).allowCondition();
     }
 }
